e4c47ed644c5909bdc0dbd6b8819ec63a7d66c3a
Big_speed90 <zulma.jeff@yahoo.com>
2023-05-01 12:55:07 -0400

    included mutations and queries to replace REST api, modified Login and Sign up, updated readMe

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -4,8 +4,13 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
 
 const LoginForm = () => {
+  const [login, { error }] = useMutation(LOGIN_USER);
+
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,15 +31,12 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      console.log(userFormData);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ I WANT to search for new books to read
 SO THAT I can keep a list of books to purchase
 ```
 
+## Live Snapshot
+[![Watch the video](./Develop/client/src/assets/Screenshot%202023-04-29%20100136.png)]
